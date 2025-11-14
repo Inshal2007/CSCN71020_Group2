bbe4fd44f47fa922ff9ba293b005d009dde88c16
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2025-11-14 11:42:16 -0500

    integrate angle calculations into main program
    - Add calculateTriangleAngles function using Law of Cosines
    - Display angles along with triangle type for valid triangles
    - Only show angles when triangle is valid (sides > 0)
    - Maintain all existing input validation and error handling

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <math.h>
 #include "triangleSolver.h"
 
 char* analyzeTriangle(int side1, int side2, int side3) {
@@ -17,8 +17,20 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 		result = "\n It is Isosceles triangle";
 	}
 	else {
-		result = "\n It isScalene triangle";
+		result = "\n It is Scalene triangle";
 	}
 
 	return result;
+}
+void calculateTriangleAngles(int side1, int side2, int side3, double* angle1, double* angle2, double* angle3) {
+	// Calculate angles using Law of Cosines
+
+	// Angle A (opposite side a)
+	*angle1 = acos((side2 * side2 + side3 * side3 - side1 * side1) / (2.0 * side2 * side3)) * (180.0 / 3.14159265358979323846);
+
+	// Angle B (opposite side b)  
+	*angle2 = acos((side1 * side1 + side3 * side3 - side2 * side2) / (2.0 * side1 * side3)) * (180.0 / 3.14159265358979323846);
+
+	// Angle C (opposite side c)
+	*angle3 = 180.0 - (*angle1 + *angle2);
 }
