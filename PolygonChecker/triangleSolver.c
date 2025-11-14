bef9b270da52e33430c532f5ce854e72164f79bb
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2025-11-13 20:23:12 -0500

    Fixed grammer in trianglesolver.c

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -6,18 +6,18 @@
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
+		result = "\n It not a Triangle";
 	}
 	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
+		result = "\n It is Equilateral triangle";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
 		(side1 == side3 && side1 != side2))
 	{
-		result = "Isosceles triangle";
+		result = "\n It is Isosceles triangle";
 	}
 	else {
-		result = "Scalene triangle";
+		result = "\n It isScalene triangle";
 	}
 
 	return result;
