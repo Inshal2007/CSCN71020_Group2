be32c5e5a0c6c73902389a808d68839595f13ad9
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2025-11-20 15:25:44 -0500

    added header file and source file for rectangle

---
+++ PolygonChecker/rectangleSolver.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "rectangleSolver.h"
+
+char* analyzeRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    return "Rectangle analysis to be implemented";
+}
+
+void getRectanglePoints(int* xCoords, int* yCoords) {
+    // Coordinate input to be implemented in next commit
+}
