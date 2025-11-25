1ed2ceb8369a63f7d4deb82c2382a152e3761475
Inshal Zahid <inshalzahid30@gmail.com>
2025-11-25 15:08:08 -0500

    implement basic rectangle menu system and function structure

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -1,13 +1,5 @@
 #pragma once
 
-// Function to analyze if four points form a rectangle
+// Rectangle analysis function declarations
 char* analyzeRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4);
-
-// Function to get four points from user input
 void getRectanglePoints(int* xCoords, int* yCoords);
-
-// Function to validate if input is a valid integer
-bool isValidInteger(const char* input);
-
-// Function to clear input buffer
-void clearInputBuffer();
