3ec57925d9e37a164054ac8f46d4a05a1efb1589
Vincent Kacperski <Vkacperski7724@conestogac.on.ca>
2025-11-27 10:10:27 -0500

    Red: Created another failing TEST_METHOD //(11) that tests boundary side length values 1.0, 99999.0 and 99999.0. Assumed that all angles 0, 0 and 0 would be returned.

--- polygonChecker.Outcomes/polygonChecker.Outcomes.cpp
+++ polygonChecker.Outcomes/polygonChecker.Outcomes.cpp
@@ -179,9 +179,23 @@ namespace polygonCheckerOutcomes
 
 		}//(10)
 
-		//TEST_METHOD(CalculateTriangleAnglesFunction_TriangleAnglesDetermination_000AnglesCase)
-		//{
-		//}//(11)
+		TEST_METHOD(CalculateTriangleAnglesFunction_TriangleAnglesDetermination_BoundaryCase)
+		{
+			//Decleration
+			double side1 = 1.0;
+			double side2 = 9999.0;
+			double side3 = 9999.0;
+			double angle1 = 0.0;
+			double angle2 = 0.0;
+			double angle3 = 0.0;
+
+			//Calculations and Comparison
+			calculateTriangleAngles(side1, side2, side3, &angle1, &angle2, &angle3);
+			Assert::AreEqual(0, angle1, 0.01);
+			Assert::AreEqual(0, angle2, 0.01);
+			Assert::AreEqual(0, angle3, 0.01);
+		
+		}//(11)
 
 		//TEST_METHOD(CalculateTriangleAnglesFunction_TriangleAnglesDetermination_NegtaiveAnglesCase)
 		//{
